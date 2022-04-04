1596e0133cca5298e7b493ebe346cc4d0785efa4
Muthukarthick T <98314834+Muka36@users.noreply.github.com>
2022-04-04 22:19:06 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+import React from "react"
+import "./style.css"
+
+const App = () => {
+    return  (
+        <div>
+            <h1 style={{textAlign:"center"}}>Hello World</h1>
+            <button className="button">App store</button>
+            <button className="button">Play store</button>
+        </div>
+    )
+}
+
+
+export default App;
